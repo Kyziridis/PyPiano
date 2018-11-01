5ef994b7cf30e35001270c40de9d9d1552a4285c
G.Kyzirids <giorgos.zapata@gmail.com>
2018-11-01 16:42:46 +0100

    Fixing run.sh

--- run.sh
+++ run.sh
@@ -1,14 +1,21 @@
-\echo "Welcome to my Piano\n";
-echo "Installing requirments....\n";
-read -p "Are you using Anaconda-python Distribution? >_ [y/n]" answer
-if [ $answer == y ]
+echo "Welcome to my Piano";
+echo "Installing requirments....";
+echo "Please type your sudo_password where is needed";
+read -p "Some essential libraries will be installed, contiue? >_ [y/n]" answer
+if [$answer==y] 
 then
-	conda install -c anaconda tk
-	conda install -c conda-forge tk 
-	conda install -c cogsci pygame
-else
 	sudo apt-get install python3-tk
 	sudo apt-get install -y python3-tk
 	python3 -m pip install -U pygame --user
-fi
+else
+	read -p "Abort Installation? >_ [y/n]" answer_2
+	if [$answer_2==y]
+	then
+		exit 1;
+	else
+		sudo apt-get install python3-tk
+		sudo apt-get install -y python3-tk
+		python3 -m pip install -U pygame --user
+	fi
+fi		
 python Mypiano.py
